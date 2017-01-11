d0b6715927ae51551e099aed1113e5c5ddd5ce1a
BrandonEckert <brandonjeckert@gmail.com>
2017-01-10 22:34:13 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# KillTheGibson
-KillTheGibson - Recreated script based on output from #OpNASADrones pastebin
+# HackTheGibson
+
+Recreated script based on output from #OpNASADrones pastebin. This WILL destroy all linux logs (not just remove symlinks)!!
+
+## Installation:
+<pre>git clone https://github.com/BrandonEckert/KillTheGibson.git && cd KillTheGibson/</pre>
+## Usage:
+<pre>sudo ./KillTheGibson.sh</pre>
---
+++ killthegibson.sh
@@ -0,0 +1,81 @@
+gen_banner() {
+cat <<"banner"
+
+                          | |                                  #   //
+                          | |                                   # //
+                          | |                ______            # //
+                          | |            ,'"       "-._        #//
+                          | |          ,'              "-._ _./#/
+                          | |         ;              __,-'/   |
+                          | |        ;|           ,-' _,'"'._,.#
+     ____________         | |         |:            _,'      |\ `.
+    /\_x________/\        | |         : \       _,-'         | \  `.
+   / /\________/\ \       | |          \ \   ,-'             |  \   \
+  / / /\______/x \ \      | |           \ '.                 |       \
+ / / / /\____/\ \ \ \     | |            \  \                |        :
+/ / / /x/\  /\ \ \ \ \    | |             `. `.              |        |
+/_x_/_/_/__\/x_\_\_\_\_\   | |               `. "-._          |        ;
+\ \ \ \ \  /\  / / / x /   | |                / |`._ `-._     |        /
+\ \ \ \ \/__\/ / / / /    | |               /  | \ `._   "-.___    _,'
+ \ \x\ \/____\/ / / /     | |              /   |  \_.-"-.___   """"
+  \ \ \/______\/ / /      | |              \   :            /"""
+   \ \/______x_\/ /       | |            `._\_       __.'_
+    \/__________\/        | |          __,--''_ ' "--'''' \_  `-._
+                          | |    __,--'     .' /_  |   __. `-._   `-._
+                          | |   <            `.  `-.-''  __,-'     _,-'
+\!/ TARGETING FILES \!/   | |    `.            `.   _,-'"      _,-'
+                          | |      `.            ''"       _,-'
+     Please wait...       | |        `.                _,-'
+                          | |          `. SATCOMS  _,-'
+                          | |            `.   __,'"
+                          |_|          
+banner
+}
+
+gen_banner
+
+# stty -echo
+# echo "*ring ring* 'Hello?'"
+# echo "KILL THE GIBSON!!!"
+# echo "\!/ TARGETING FILES \!/ "
+# dd if="/dev/null" of=".../tmp/logs"
+# echo "Deleted [+].../tmp/logs"
+# dd if="/dev/null" of=".../root/.bash_history"
+# echo "Deleted [+].../root/.bash_history"
+# dd if="/dev/null" of=".../root/.ksh_history"
+# echo "Deleted [+].../root/.ksh_history"
+# dd if="/dev/null" of=".../root/.bash_logout"
+# echo "Deleted [+].../root/.bash_logout"
+# dd if="/dev/null" of=".../usr/local/apache/logs"
+# echo "Deleted [+].../usr/local/apache/logs"
+# dd if="/dev/null" of=".../usr/local/apache/log"
+# echo "Deleted [+].../usr/local/apache/log"
+# dd if="/dev/null" of=".../var/apache/logs"
+# echo "Deleted [+].../var/apache/logs"
+# dd if="/dev/null" of=".../var/apache/log"
+# echo "Deleted [+].../var/apache/log"
+# dd if="/dev/null" of=".../var/run/utmp"
+# echo "Deleted [+].../var/run/utmp"
+# dd if="/dev/null" of=".../var/logs"
+# echo "Deleted [+].../var/logs"
+# dd if="/dev/null" of=".../var/log"
+# echo "Deleted [+].../var/log"
+# dd if="/dev/null" of=".../var/adm"
+# echo "Deleted [+].../var/adm"
+# dd if="/dev/null" of=".../etc/wtmp"
+# echo "Deleted [+].../etc/wtmp"
+# dd if="/dev/null" of=".../etc/utmp"
+# echo "Deleted [+].../etc/utmp"
+# dd if="/dev/null" of="...$HISTFILE"
+# echo "Deleted [+]...$HISTFILE"
+# dd if="/dev/null" of=".../var/log/lastlog"
+# echo "Deleted [+].../var/log/lastlog"
+# dd if="/dev/null" of=".../var/log/wtmp"
+# echo "Deleted [+].../var/log/wtmp"
+# history -c
+
+# echo ""
+# echo ".."
+# echo "...//"
+# echo "YOUR TRACES HAVE BEEN SUCCESSFULLY ERASED FROM THE SERVER! ─=≡Σ(/.__.)/"
+# stty echo
